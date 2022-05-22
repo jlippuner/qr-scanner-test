842d4f7d05aa1ce63543241e7c3f0facee70fdc7
Jonas Lippuner <jonas@lippuner.ca>
2022-05-21 23:17:33 -0500

    fix Clipboard

--- app/assets/app.js
+++ app/assets/app.js
@@ -25,7 +25,7 @@ window.addEventListener('DOMContentLoaded', function () {
         flipCameraButton.disabled = false;
         scanCode(true);
     });
-    new Clipboard('dialog button.copy');
+    new ClipboardJS('dialog button.copy');
 
     // init QRCode Web Worker
     const qrcodeWorker = new Worker("assets/qrcode_worker.js");
